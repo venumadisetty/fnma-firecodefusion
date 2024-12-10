bf1a82cef3c339a402520ac00244baf72a34f660
malharazi34 <malharazi34@gmail.com>
2024-12-10 14:45:12 -0500

    Added Pagination to Table

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {SamsearchService} from "../app.service";
 import { HttpClient } from '@angular/common/http';
+import {PageEvent} from "@angular/material/paginator";
 
 
 
@@ -12,14 +13,17 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   showSearchResults = false;
-  tableData: any[] = [];
   title = 'SAM Search API';
+  pagedResults = [];
+  pageSize = 10;
+  currentPage = 0;
   samSearchResults: any[] = [];
   nextToken: string | null = null;
   selectedRadio: any
 
 
   ngOnInit() {
+      this.updatePagedResults();
 
   }
   basicSearch = {
@@ -49,7 +53,6 @@ export class HomeComponent implements OnInit {
   onBasicSearchSubmit() {
     console.log('Basic Search Submitted:', this.basicSearch);
     this.fetchSAMSearchData();
-    this.loadMockData();
     this.showSearchResults = true;
     this.basicSearch = { samInput: '', filters: {
         sam: '',
@@ -65,35 +68,35 @@ export class HomeComponent implements OnInit {
 
   onSemanticSearchSubmit() {
     console.log('Semantic Search Submitted:', this.semanticSearch.text);
-    this.loadMockData();
     this.showSearchResults = true;
 
   }
 
-  // Fetch mock data from JSON
-  loadMockData() {
-    this.http.get<any[]>('assets/mock-data.json').subscribe(
-        (data) => {
-          console.log('Loaded Data:', data);
-          this.tableData = data; // Set the data to display
-        },
-        (error) => {
-          console.error('Error loading mock data:', error);
-        }
-    );
+
+  onPageChange(event: PageEvent) {
+      this.currentPage = event.pageIndex;
+      this.pageSize = event.pageSize;
+      this.updatePagedResults();
   }
 
+    updatePagedResults() {
+        const startIndex = this.currentPage * this.pageSize;
+        const endIndex = startIndex + this.pageSize;
+        this.pagedResults = this.samSearchResults.slice(startIndex, endIndex);
+    }
+
   fetchSAMSearchData() {
-    console.log(this.selectedRadio)
-    console.log(this.basicSearch.samInput)
     this.samSearchService.listSAMDATA(this.selectedRadio, this.basicSearch.samInput).subscribe(
         (response: any) => {
           this.samSearchResults = response.data.listSAMDATA.items;
           this.nextToken = response.data.listSAMDATA.nextToken;
+          this.currentPage=0;
+          this.updatePagedResults();
         },
         (error: any) => {
           console.error('Error fetching SAMSearch data:', error);
         }
     );
+
   }
 }
